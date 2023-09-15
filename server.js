f33c27560d6eff4ca94f105e9522a7e62811e085
krishna9802 <krishna@artenos.io>
2023-09-15 20:35:42 +0530

    uploading on render

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
